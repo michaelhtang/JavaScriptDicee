46d5caeb88ab1f76d07c1e1726089a094024f552
Michael Tang <michael.h.tang@gmail.com>
2020-07-30 14:49:07 -0600

    Add flag emoji
    Signed-off-by: Michael Tang <michael.h.tang@gmail.com>

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ function compareNumbers() {
     document.querySelector("h1").textContent = "Draw";
   }
   if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").textContent = "Player 1 Wins";
+    document.querySelector("h1").textContent = "🚩Player 1 Wins";
   }
   if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").textContent = "Player 2 Wins";
+    document.querySelector("h1").textContent = "Player 2 Wins🚩";
   }
 }
+
 randomNumber()
 compareNumbers()
